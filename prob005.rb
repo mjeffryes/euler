6640fa31491cc456160c1b568395991f5daf8221
Matthew Jeffryes <matt@mjeffryes.net>
2012-01-09 17:58:58 -0800

    problem 5, attempt 1

---
+++ prob005.rb
@@ -0,0 +1,12 @@
+require "../prob003/attempt1.rb"
+
+factors = {}
+
+for i in 1..20 do
+	fl = listfactors( i )
+	fh = Hash[fl.map{ |x| [x, fl.count(x)] }]
+	factors.update(fh){|key, v1, v2| [v1, v2].max }
+end
+n = 1
+factors.each{ |k,v| n *= k**v }
+puts n
