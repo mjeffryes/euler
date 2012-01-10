b63bbb94043b1eeba337480cffaeb8b6449ce37c
Matthew Jeffryes <matt@mjeffryes.net>
2012-01-09 17:58:13 -0800

    problem 4, attempt 3

--- prob004.rb
+++ prob004.rb
@@ -1,7 +1,27 @@
-n = 9*2
-while n > 0 do
-	for i in (((n+1)/2)..9)
-		puts i*(n-i)
+require 'generator'
+def gen_dec_multiples( max )
+	return  Generator.new { |g| 
+	n = max*2
+	while n > 0 do
+		for i in (((n+1)/2)..max)
+			g.yield i*(n-i)
+		end
+		n -= 1
 	end
-	n -= 1
+	}
 end
+
+def is_palindrome(v)
+	s = v.to_s
+	for i in 0..((s.length-1)/2) do
+		return false if s[i] != s[-(i+1)]
+	end
+	return true
+end
+
+v = nil
+g = gen_dec_multiples(999)
+begin
+	v = g.next
+end while not is_palindrome(v)
+	puts v
