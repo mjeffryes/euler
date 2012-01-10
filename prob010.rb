3de95e23a73e46ee4af90386d7eff76834cde078
Matthew Jeffryes <matt@mjeffryes.net>
2012-01-09 18:09:22 -0800

    problem 10, attempt 2

--- prob010.rb
+++ prob010.rb
@@ -1,9 +1,33 @@
-require 'prob007.rb'
+require 'generator'
+
+def prime_gen()
+	return  Generator.new { |g| 
+		primes = []
+		n = 2
+		while 1 do
+			root = Math.sqrt(n).to_i + 1
+			prime = true
+			for p in primes do
+				break if p > root
+				if n%p == 0  then
+					prime = false
+					break
+				end
+			end
+			if prime then
+				primes << n 
+				g.yield n
+			end
+			n += 1
+		end
+	}
+end
+
 g = prime_gen()
 n = 0
 sum = 0
 while n < 2000000 do
-	n = g.next
 	sum += n
+	n = g.next
 end
 puts sum
