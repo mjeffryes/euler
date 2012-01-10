fb050874b284c62f221ff621c11b4f5d2f927a2a
Matthew Jeffryes <matt@mjeffryes.net>
2012-01-09 17:55:20 -0800

    problem 3, attempt 1

---
+++ prob003.rb
@@ -0,0 +1,14 @@
+def listfactors( value )
+	list = []
+	factor = 2
+	while factor < value do
+		if( value % factor == 0 )
+			list << factor 
+			value /= factor
+		else
+			factor = factor + 1
+		end
+	end
+	list << value
+	return list
+end
