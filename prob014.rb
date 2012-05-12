28cfda67b3c62a4f35116ee49344ca635cdaf30a
Matthew Jeffryes <matt@mjeffryes.net>
2012-05-12 14:27:40 -0700

    prob014 cached recursive solution

--- prob014.rb
+++ prob014.rb
@@ -1,18 +1,22 @@
 
+$cache = {}
 def chain_len( n )
 	return 1 if n == 1
-	if n%2 == 0
-		return chain_len( n/2 ) + 1 
-	else
-		return chain_len( 3*n + 1 ) + 1
-	end
+	return $cache[n] if $cache.has_key?(n)
+	len = ( n%2 == 0 ? chain_len( n/2 ) : chain_len( 3*n + 1 ) ) + 1
+	$cache[n] = len
+	return len
 end
 
 def prob014()
 	max = 1
+	nmax = 1
 	for n in 2..1000000
 		l = chain_len( n )
-		max = l if l > max
+		if l > max
+			max = l
+			nmax = n
+		end
 	end
-	print max
+	p nmax => max
 end
